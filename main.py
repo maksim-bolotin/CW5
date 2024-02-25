21f31fedd185fdae2ddf052b9be699a3312a6274
Maksim Bolotin <vk07122012@gmail.com>
2024-02-25 21:04:12 +0300

    Создание основного кода проекта, содержит список работодателей, параметры для подключения к БД, а также вызов метода insert_vacancies_from_json для экземпляра класса DBManager.

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from BD.db_manager import DBManager
+from BD.head_hunter import get_hh_data
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+    # параметры подключения к базе данных PostgreSQL.
+    try:
+        db_params = {'dbname': 'mydatabase', 'user': 'postgres',
+                     'password': 'sqlUSER', 'host': 'localhost'}
+    # список компаний.
+        companies = ['Яндекс', 'Mail.Ru Group', 'Лаборатория Касперского', 'Luxoft', 'Сбер',
+                     'OCS', '3Logic Group', 'Sitronics Group', 'Rubytech', 'Cloud.ru']
+
+        db_manager = DBManager(**db_params)
+        for company in companies:
+            data = get_hh_data(company)
+            if data:
+                db_manager.insert_vacancies_from_json(company, f'{company}_vacancies.json', )
+
+        # Закрытие соединения с базой данных
+        db_manager.close_connection()
+    except Exception as e:
+        print(f"{e}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
