2df439bb6b5f1aa036bb3bf22a74e6bb3167c9bb
Maksim Bolotin <vk07122012@gmail.com>
2024-02-26 20:38:49 +0300

    Переделал функцию подключения к hh.ru, ранее данные сохранялись в json файл.

--- BD/head_hunter.py
+++ BD/head_hunter.py
@@ -1,8 +1,10 @@
-import json
 import requests
 
 
 def get_hh_data(employer_name):
+    """
+    Функция подключения к "api.hh.ru".
+    """
     url = 'https://api.hh.ru/vacancies'
     params = {'employer_name': employer_name}
     try:
@@ -10,9 +12,7 @@ def get_hh_data(employer_name):
         response.raise_for_status()
         if response.status_code == 200:
             data = response.json().get('items', [])
-            with open(f'{employer_name}_vacancies.json', 'w', encoding='utf-8') as file:
-                json.dump(data, file, ensure_ascii=False, indent=2)
             return data
     except requests.exceptions.RequestException as e:
         print(f"{e}")
-    return None
+        return None
