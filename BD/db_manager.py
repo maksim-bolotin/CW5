dc5821cbe101f3b84458aac487a07bea5ab75c6e
Maksim Bolotin <vk07122012@gmail.com>
2024-02-27 00:31:25 +0300

    дописаны методы get_vacancies_with_higher_salary, get_avg_salary. запись данных в БД вынесена из класса в utils.py

--- BD/db_manager.py
+++ BD/db_manager.py
@@ -1,49 +1,18 @@
-import json
 import psycopg2
+from BD.db_config import db_params
 
 
 class DBManager:
-    """
-    Класс для работы с данными в БД.
-    """
-    def __init__(self, dbname, user, password, host):
-        self.dbname = dbname
-        self.user = user
-        self.password = password
-        self.host = host
+    def __init__(self, **db_params):
         self.conn = None
         self.cursor = None
         self.connect()
 
     def connect(self):
-        self.conn = psycopg2.connect(dbname=self.dbname, user=self.user, password=self.password, host=self.host)
+        self.conn = psycopg2.connect(**db_params)
         self.cursor = self.conn.cursor()
 
-    def insert_vacancies_from_json(self, company, json_filename):
-        with open(json_filename, 'r', encoding='utf-8') as file:
-            vacancies = json.load(file)
-
-        for vacancy in vacancies:
-            title = vacancy.get('name', '')
-            description = vacancy.get('description', '')
-            salary_from = vacancy.get('salary_from', '')
-            salary_to = vacancy.get('salary_to', '')
-            url = vacancy.get('url', '')
-
-            self.cursor.execute("SELECT employers_id FROM employers WHERE employers_name = %s;", (company,))
-            employer_id = self.cursor.fetchone()
-            if not employer_id:
-                self.cursor.execute("INSERT INTO employers (employers_name) VALUES (%s) RETURNING employers_id;", (company,))
-                employer_id = self.cursor.fetchone()[0]
-            else:
-                employer_id = employer_id[0]
-            self.cursor.execute("""
-                INSERT INTO vacancies (employers_id, title, salary_from, salary_to, url, description)
-                VALUES (%s, %s, %s, %s, %s, %s);
-                """, (employer_id, title, salary_from, salary_to, url, description))
-
     def get_companies_and_vacancies_count(self):
-        # получает список всех компаний и количество вакансий у каждой компании.
         try:
             query = """
                 SELECT employers.employers_name, COUNT(vacancies.id_vacancies) as vacancy_count
@@ -52,9 +21,12 @@ class DBManager:
                 GROUP BY employers.employers_name;
             """
             self.cursor.execute(query)
-            return self.cursor.fetchall()
+            result = self.cursor.fetchall()
+            print("Query result:", result)
+            return result
         except psycopg2.Error as e:
-            print(f"ошибка получения списка компаний и количества вакансий: {e}")
+            print(f"Ошибка получения списка компаний и количества вакансий: {e}")
+            return None
 
     def get_all_vacancies(self):
         # получает список всех вакансий с указанием названия компании,
@@ -73,7 +45,11 @@ class DBManager:
     def get_avg_salary(self):
         # получает среднюю зарплату по вакансиям.
         try:
-            query = ""
+            query = """
+                SELECT AVG(CAST(salary_from AS NUMERIC)) AS avg_salary
+                FROM vacancies
+                WHERE salary_from IS NOT NULL;
+            """
             self.cursor.execute(query)
             return self.cursor.fetchone()[0]
         except psycopg2.Error as e:
@@ -83,7 +59,12 @@ class DBManager:
         # получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
         try:
             avg_salary = self.get_avg_salary()
-            query = ""
+            query = """
+                SELECT employers.employers_name, vacancies.title, vacancies.salary_from, vacancies.url
+                FROM employers
+                INNER JOIN vacancies ON employers.employers_id = vacancies.employers_id
+                WHERE vacancies.salary_from > {avg_salary};
+            """
             self.cursor.execute(query)
             return self.cursor.fetchall()
         except psycopg2.Error as e:
